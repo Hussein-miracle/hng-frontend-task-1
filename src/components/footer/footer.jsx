d03e273e5f7cb797f7a0579e46ee131333ddcc34
Abdullahi <miraacle64@gmail.com>
2022-10-28 09:42:28 +0100

    feat: final commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,16 +1,24 @@
 import "./footer.scss";
 import zuri from "../../assets/Zuri.Internship_Logo.svg";
 import ingressive from "../../assets/ingressive.svg";
-
+import github from "../../assets/github-icon.svg";
+import slack from "../../assets/slack-icon.svg";
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <img src={zuri} alt="zuri_logo" />
-      <span>HNG Internship 9 Frontend Task</span>
-      <img src={ingressive} alt="ingressive" />
+    <footer className="footer">
+      <div className="footer__header">
+        <a href="#" alt="slack-link"> <img src={slack} alt="slack_icon"/> </a>
+        <a href="https://github.com/Hussein-miracle" alt="github_link"> <img src={github} alt="github_icon"/> </a>
+      </div>
 
+      <div className="footer__main">
+        <a><img src={zuri} alt="zuri_logo" /></a>
+        
+        <span>HNG Internship 9 Frontend Task</span>
+        <a><img src={ingressive} alt="ingressive" /></a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,35 @@
 import "./header.scss";
 import profileImg from "../../assets/profile__img.png";
 import cam from "../../assets/camera-icon.svg";
+import shareBtn from "../../assets/share-btn.svg";
+import menuBtn from "../../assets/menu-btn.svg";
+
+import ReactTooltip from 'react-tooltip';
 
 const Header = () => {
   return (
-    <header className='header'>
-
+    <header className="header">
       
-      <div className="header__img--container">
-      <img alt="profile_img"  src={profileImg} className="header__img"/>
+        <div className="header__share" data-tip="Share Link">
+          <img src={`${shareBtn}`}  className="share_btn"/>
+          <img src={menuBtn} className="menu_btn" />
+        </div>
 
-      <div className="header__hover-effect">
-        <img src={cam} alt="camera-icon" className=".img"/>
+
+      <div className="header__img--container">
+        <img alt="profile_img" src={profileImg} className="header__img" />
+
+        <div className="header__hover-effect">
+          <img src={cam} alt="camera-icon" className="svg" />
+        </div>
       </div>
-      </div>
-      <span className="header__name">Abdullahi.ts</span>
-      <span aria-hidden={true} id="slack">hussein_miracle</span>
+      <span className="header__name">Abdullahi.ts✨🚀</span>
+      <span aria-hidden={true} id="slack">
+        hussein_miracle
+      </span>
+      <ReactTooltip/>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
